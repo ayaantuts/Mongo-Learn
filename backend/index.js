d4ee15fa15b31ae928de0c1a1aabc82945e14a50
Ayaan Shaikh <ayaanmaths04@gmail.com>
2025-03-09 10:44:56 +0530

    chore: Bumped up the version of the backend

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const productRoutes = require('./routes/products.routes');
 dotenv.config();
 
